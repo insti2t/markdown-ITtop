549d0b0536c1483a11d2722801dc7278ac0ff2ef
insti2t <insti2t.mail@gmail.com>
2026-09-15 15:51:53 +0300

    Add files via upload

---
+++ project/005/пример.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+typedef unsigned long long ull_num; // псевдоним
+
+// гл функция
+int main() {
+    
+    cout << "a + (m - b)" << endl;
+
+    int a;
+    cout << "Type A" << endl;
+    cin >> a;
+    int b;
+    cout << "Type B" << endl;
+    cin >> b;
+    int m;
+    cout << "Type M" << endl;
+    cin >> m;
+    int c = m - b;
+
+    cout << "a=" << a << ", b=" << b << ", m=" << m << endl;
+
+    cout << "Answer: " << a + c << endl;
+
+    return 0;
+}
